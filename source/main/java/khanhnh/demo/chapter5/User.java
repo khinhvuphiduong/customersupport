94f2aa669be1a055665131963fad1517d3388e68
Nguyen Huy Khanh <khanhnguyenhuy@hotmail.com>
2016-07-04 16:23:24 +0700

    update

---
+++ source/main/java/khanhnh/demo/chapter5/User.java
@@ -0,0 +1,56 @@
+package khanhnh.demo.chapter5;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class User {
+	public User(){
+		
+	}
+	
+	public User(long userId, String username, String firstName, String lastName)
+	{
+		this.userId = userId;
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	private long userId;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private Map<String, Boolean> permissions=new Hashtable<>();
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Map<String, Boolean> getPermissions() {
+		return permissions;
+	}
+	public void setPermissions(Map<String, Boolean> permissions) {
+		this.permissions = permissions;
+	}
+	
+	
+}
